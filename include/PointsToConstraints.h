5c90b50632a864aeac6500708a02c23201b2b6dc
Prince Dhaliwal <dhaliwalprince@hotmail.com>
2017-02-15 14:17:12 +0530

    Added mapping for instructions to constraints

--- include/PointsToConstraints.h
+++ include/PointsToConstraints.h
@@ -19,6 +19,13 @@ public:
         constraints_.push_back(Constraint(src, dest, type));
     }
 
+    void insert(NodeIndex src, NodeIndex dest, ConstraintType type,
+        llvm::Value *val) {
+        auto idx = constraints_.size();
+        insert(src, dest, type);
+        irIndex_.insert(val, idx);
+    }
+
     iterator begin() {
         return constraints_.begin();
     }
@@ -40,8 +47,21 @@ public:
     // returns the vector of pointees of idx
     std::set<NodeIndex> getPointees(NodeIndex idx) const;
 
+    // get constraint for particular instruction i
+    // returns nullptr if there is no constraint for this instruction
+    Constraint *get(llvm::Value *i) {
+        auto it = irIndex_.find(i);
+        if (it == irIndex_.end()) {
+            return nullptr;
+        }
+
+        return &constraints_[it->second];
+    }
 private:
     std::vector<Constraint> constraints_;
+
+    // to allow querying using llvm instruction
+    std::map<llvm::Value*, int> irIndex_;
 };
 
 }
